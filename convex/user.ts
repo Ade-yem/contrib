b6e6dc1507f2837590954ea76434ca7927686898
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2024-08-16 20:55:22 +0100

    feat: completed the basic payment and subscirption functions

--- convex/user.ts
+++ convex/user.ts
@@ -1,5 +1,6 @@
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
+import { v } from "convex/values";
 
 export const getUser = query({
     args: {},
@@ -8,3 +9,27 @@ export const getUser = query({
         return userId !== null ? ctx.db.get(userId) : null;
     },
 });
+
+export const getInviteLink = query({
+    args: {
+        group_id: v.id("groups")
+    },
+    async handler(ctx, args_0) {
+        const res = await ctx.db.query("invites").filter((inv) => inv.eq(inv.field("group_id"), args_0.group_id)).collect();
+        const inv_code = res[0].code
+        const link = process.env.SITE_URL;
+        return `${link}/inv_code`
+    },
+})
+
+export const editProfile = mutation({
+    args: {
+        image: v.optional(v.string()),
+        name: v.optional(v.string()),
+        phone: v.optional(v.string()),
+        user_id: v.id("users")
+    },
+    async handler(ctx, args_0) {
+        await ctx.db.patch(args_0.user_id, {image: args_0.image, name: args_0.name, phone: args_0.phone})
+    }
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,56 @@ import { SignInWithPassword } from "@/components/forms/SigninForm";
 import { Authenticated, Unauthenticated, useQuery, useAction, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import Button from "@/components/buttons/BaseButton";
+import { Id } from "../../convex/_generated/dataModel"
+import { useState } from "react";
 
 export default function Home() {
   const user = useQuery(api.user.getUser);
+  const [group_id, setGroup_id] = useState<Id<"groups"> | "">("");
   const initializeTransaction = useAction(api.payments.initializePaystackTransaction);
   const createPlan = useAction(api.payments.createPaystackPlan);
-  const createSubscription = useAction(api.payments.createSubscription);
-  const joinGroup = useMutation(api.group.createGroup)
+  // const createSubscription = useAction(api.payments.createSubscription);
+  const addGroup = useMutation(api.group.createGroup);
+  const createInvite = useMutation(api.group.createInvite);
+  const joinGroup = useMutation(api.group.createMembership);
 
   const createGroup = async () => {
-
+    const group_id = await addGroup({
+      creator_id: user?._id as Id<"users">,
+      name: "Adeyemi Adejumo",
+      number_of_people: 5,
+      number_of_people_present: 0,
+      interval: "monthly",
+      savings_per_interval: 10000,
+      subscription_plan_id: "",
+      status: "pending",
+      start_date: 0,
+      elapsed_time: 0,
+      private: false
+    })
+    if (group_id) {
+      const plan = await createPlan({
+        name: "First test",
+        group_id: group_id as Id<"groups">,
+        amount: 10000,
+        description: "First paystack test",
+        currency: "NGN",
+        invoiceLimit: 5,
+        email: user?.email as string
+      })
+      console.log(plan);
+      const invite_code = await createInvite({
+        status: "pending",
+        group_id: group_id
+      })
+      console.log(invite_code);
+    }
   }
   const payMoney = async () => {
-
+    const res = await initializeTransaction({amount: 10000, email: user?.email as string})
+    if (res) {
+      window.location.href = res.data.authorization_url;
+    }
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
