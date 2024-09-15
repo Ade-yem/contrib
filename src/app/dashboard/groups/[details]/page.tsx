3ee5bede0559ba0adbb0071bcb88809e11b80e66
learnzie <oluwatola15@gmail.com>
2024-09-15 22:56:33 +0100

    :zap:

--- src/app/dashboard/groups/[details]/page.tsx
+++ src/app/dashboard/groups/[details]/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
 import Loader from "@/components/shared/Loader";
@@ -56,7 +55,185 @@ export default function GroupDetails({
         {GroupDetails?.nameOfGroup}
       </h1>
       <br />
-      <div className="row">
+
+      {/* Tab navigation */}
+      <ul className="nav nav-tabs d-lg-none" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="overview-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#overview"
+            type="button"
+            role="tab"
+            aria-controls="overview"
+            aria-selected="true"
+          >
+            Overview
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="chat-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#chat"
+            type="button"
+            role="tab"
+            aria-controls="chat"
+            aria-selected="false"
+          >
+            Group Chat
+          </button>
+        </li>
+      </ul>
+
+      {/* Tab content */}
+      <div className="tab-content d-lg-none" id="myTabContent">
+        {/* Overview Tab Pane */}
+        <div
+          className="tab-pane fade show active"
+          id="overview"
+          role="tabpanel"
+          aria-labelledby="overview-tab"
+        >
+          <div className="row">
+            <div className="col-12">
+              <div className="row">
+                <div className="col-9 h-auto bg-white-000 rounded-10 p-4_5 d-flex justify-content-between">
+                  <div>
+                    <p className="text-gray-400 text-sm">Available Savings</p>
+                    <p className="text-sm mb-4">
+                      &#8358;
+                      {visible
+                        ? thousandFormatter(GroupDetails?.available ?? 0)
+                        : "*****"}
+                    </p>
+                    <p className="text-sm text-primary-500 fw-bold mb-0">
+                      Next Collector:{" "}
+                      <span className="text-black-000">
+                        {GroupDetails?.nextReceiver}
+                      </span>
+                    </p>
+                  </div>
+                  <div
+                    className="my-auto click"
+                    onClick={() => setVisible((prev) => !prev)}
+                  >
+                    {!visible ? (
+                      <Icon
+                        icon="weui:eyes-on-outlined"
+                        width="2.5rem"
+                        height="2.5rem"
+                        role="button"
+                      />
+                    ) : (
+                      <Icon
+                        icon="iconamoon:eye-off-thin"
+                        width="2.5rem"
+                        height="2.5rem"
+                        role="button"
+                      />
+                    )}
+                  </div>
+                </div>
+                <div className="col-3 h-auto d-flex gap-2 flex-column justify-content-between">
+                  <div className="bg-purple rounded-10 h-100 p-2 d-flex align-items-center gap-3">
+                    <p className="text-white-000 text-2xs mb-0">
+                      Collection Cycle:
+                    </p>
+                    <p className="text-white-000 text-2xs mb-0">
+                      {GroupCollectionPercentage}%
+                    </p>
+                  </div>
+                  <div className="bg-white-000 rounded-10 h-100 p-2 d-flex flex-column justify-content-between">
+                    <p className="text-red text-center text-2xs mb-2 fw-bold">
+                      Defaulters of the Day
+                    </p>
+                    {GetDefaultersDetails?.map((defaulter, index) => (
+                      <p className="text-3xs mb-0" key={index}>
+                        {defaulter.amount}
+                        {defaulter.status}
+                        {defaulter.userId}
+                      </p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="bg-white-000 rounded-10 col-5 p-4">
+                  <p className="text-lg fw-bold text-center mb-4">
+                    Group Members
+                  </p>
+                  <div className="webkit-scrollbar-none overflow-auto details-container">
+                    {GroupMembershipsDetails?.map((members, index) => (
+                      <div
+                        key={index}
+                        className="d-flex align-items-center gap-3 mb-3 "
+                      >
+                        <Image
+                          src={members.image ?? "/avatar.svg"}
+                          width={40}
+                          height={40}
+                          alt="profile-pics"
+                          className="rounded-circle"
+                        />
+                        <p className="text-xs fw-bold mb-0"> {members.name}</p>
+                        {members.creator && (
+                          <Icon
+                            icon="openmoji:crown"
+                            width="2rem"
+                            height="2rem"
+                          />
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="col-7 ">
+                  <div className="bg-white-000 rounded-10 p-4">
+                    <p className="text-lg fw-bold text-center mb-4">
+                      Recent Activities
+                    </p>
+                    <div className="webkit-scrollbar-none overflow-auto details-container">
+                      {GetRecentActivity?.map((activity, index) => (
+                        <div
+                          className="d-flex justify-content-between gap-3 mb-3 "
+                          key={index}
+                        >
+                          <div>
+                            <p className="text-xs text-primary-500 fw-bold mb-0">
+                              {activity.details}
+                            </p>
+                          </div>
+                          <p className="text-xs fw-bold mb-0 text-green">
+                            + &#8358;{thousandFormatter(activity.amount ?? 0)}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Group Chat Tab Pane */}
+        <div
+          className="tab-pane fade"
+          id="chat"
+          role="tabpanel"
+          aria-labelledby="chat-tab"
+        >
+          <div className="bg-white-000 rounded-10 p-4 ">
+            <p className="text-xl fw-bold text-primary-500 mb-">Group Chats</p>
+            <GroupChat groupId={groupId as Id<"groups">} userId={user!?._id} />
+          </div>
+        </div>
+      </div>
+
+      <div className="row d-none d-lg-flex">
         <div className="col-lg-7 col-12">
           <div className="row">
             <div className="col-9 h-auto bg-white-000 rounded-10 p-4_5 d-flex justify-content-between">
